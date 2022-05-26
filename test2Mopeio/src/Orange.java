8032747c1e500a6ddc57a9739dd6aeb0d10c3569
1856165 <1856165@01-19961.pusd.dom>
2022-05-26 11:05:25 -0700

    added orange2

---
+++ test2Mopeio/src/Orange.java
@@ -0,0 +1,79 @@
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.geom.AffineTransform;
+import java.net.URL;
+
+public class Orange {
+
+	public int x,y;
+	public int vy = 0;
+	private String fileName;
+	
+	//for image stuff
+	private Image img; 	
+	private AffineTransform tx;
+	private double scaleW = 1.5, scaleH = 1.5;
+	
+	public Orange() {//default constructor
+		
+	}
+	
+	public Orange(int x,int y) {//non default constructor
+		img = getImage("Orange.png");   
+		
+		this.x = x;
+		this.y = y;
+		
+		tx = AffineTransform.getTranslateInstance(x, y );
+		init(x, y); 				//initialize the location of the image
+	}
+	
+	private void init(double a, double b) {
+		tx.setToTranslation(a, b);
+		tx.scale(scaleW, scaleH);
+	}
+	
+	public int getX() {//getter (accessor)
+		return x;
+	}
+	public int getY() {
+		return y;
+	}
+	
+	public void setX(int newX) {//setter (mutator)
+		x = newX;
+	}
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	private Image getImage(String path) {
+		Image tempImage = null;
+		try {
+			URL imageURL = Background.class.getResource(path);
+			tempImage = Toolkit.getDefaultToolkit().getImage(imageURL);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return tempImage;
+	}
+	
+	public void paint(Graphics g) {
+		//these are the 2 lines of code needed draw an image on the screen
+		Graphics2D g2 = (Graphics2D) g;
+		
+		//call update to update the actual picture location
+		update();
+		g2.drawImage(img, tx, null);
+	}
+
+
+public void update() {
+		
+	tx.setToTranslation(x, y);
+	tx.scale(1.3	, 1.3);
+	
+	}
+}
